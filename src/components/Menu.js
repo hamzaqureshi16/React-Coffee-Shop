1fbbff01788cb4c60816511623e934361cd62781
Hamza Qureshi <m.hamzaqureshi15@gmail.com>
2023-02-02 01:04:17 +0500

    partially fixed menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import cappuchino from '../assets/img/flatwhite-coffee.jpg';
+import React, { useEffect } from "react";
+import cappuchino from "../assets/img/flatwhite-coffee.jpg";
 const menuitemstyle = {
-    fontSize: '1.5rem',
-    fontWeight: 'bold',
-    color: 'white',
-    textShadow: '2px 2px 4px #000000',
-    fontFamily: 'cursive',
-    textTransform: 'uppercase',
-    letterSpacing: '0.2rem', 
-    border: 'none',
-    borderRadius: '0.5rem',
+  fontSize: "0.9rem",
+  fontWeight: "bold",
+  color: "white",
+  textShadow: "2px 2px 4px #000000",
+  fontFamily: "cursive",
+  textTransform: "uppercase",
+  letterSpacing: "0.2rem",
+  border: "none",
+  borderRadius: "0.5rem",
+};
 
-}
-const menuimgstyle = {
-    height: '5rem',
-    width: '6rem',
-    borderRadius: '0.5rem',
-    border: '1px solid black',
-    boxShadow: '2px 2px 4px #000000'
-}
+const BuyNow = () =>{};
+const AddtoCart = () =>{};
 
 export default function Menu() {
+  useEffect(() => {}, []);
+  const ProductCatalog = [
+    {
+      id: 1,
+      name: "Flat White",
+      price: "$4.50",
+      ingredients: "Espresso, steamed milk",
+      category: "starters",
+    },
+    {
+      id: 2,
+      name: "Latte",
+      price: "$4.75",
+      ingredients: "Espresso, steamed milk, foam",
+      category: "salads",
+    },
+    {
+      id: 3,
+      name: "Mocha",
+      price: "$5.00",
+      ingredients: "Espresso, steamed milk, chocolate syrup, foam",
+      category: "salads",
+    },
+  ];
+
   return (
     <div>
       <div id="menu" className="menu ">
@@ -32,32 +52,46 @@ export default function Menu() {
           <div className="row" data-aos="fade-up" data-aos-delay={100}>
             <div className="col-lg-12 d-flex justify-content-center">
               <ul id="menu-flters">
-                <li data-filter="*" className="filter-active">All</li>
+                <li data-filter="*" className="filter-active">
+                  All
+                </li>
                 <li data-filter=".filter-starters">Coffee's</li>
                 <li data-filter=".filter-salads">Beans</li>
                 <li data-filter=".filter-specialty">Deserts</li>
               </ul>
             </div>
           </div>
-        </div></div>
-        <div style={menuitemstyle}>
-        <div className="container" data-aos="fade-up">
-            <div className="row menu-container" data-aos="fade-up" data-aos-delay={200}>
-                <div className="col-lg-6 menu-item filter-starters">
-                    <img style={menuimgstyle} src={cappuchino} className="menu-img" alt="" />
-                    <div className="menu-content">
-                        <a href="/">Cappuccino</a><span>$3.99</span>
-                        </div>
-                    <div className="menu-ingredients">
-                        A small amount of espresso with a lot of steamed milk and a light layer of foam.
-                        </div>
-                        </div>
-                        </div>
-                        </div>
-        </div>
-        
-        
-        
+          </div>
+        { ProductCatalog.map( (item)=>(
+        <div className={`col-lg-6 menu-item filter-starters`} style={menuitemstyle}>
+            <img
+              src={cappuchino}
+              className="rounded-circle"
+              height="85px"
+              width="85px"
+              class="menu-img"
+              alt=""
+            />
+            <div className="menu-content">
+              <a href="/">{item.name}</a>
+              <span>{item.price}</span>
+            </div>
+            <div className="menu-ingredients">{item.ingredients}</div>
+            &nbsp; &nbsp;
+            <button
+              className="bg-success rounded"
+              onClick={()=>BuyNow(item.id)}
+            >
+              buy
+            </button>
+            <button
+              className="bg-primary rounded"
+              onClick={()=>AddtoCart(item.id)}
+            >
+              Add to cart
+            </button>
+          </div>) )}
+      </div>
     </div>
-  )
+  );
 }
